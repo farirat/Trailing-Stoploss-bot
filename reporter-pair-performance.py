f62978959c34af06ac01ef98ad1775018b589c99
Fourat ZOUARI <fourat@gmail.com>
2020-07-07 13:33:12 +0100

    Added more metrics to pairperf report

--- reporter-pair-performance.py
+++ reporter-pair-performance.py
@@ -44,20 +44,22 @@ try:
 
     # Get pairperf_column_settings
     markets = {}
+    _skeleton = {
+            'closed_last_hour': 0,
+            'gain_at_stoploss': 0,
+            'cumulated_gains': 0,
+            'open_positions': 0,
+            'opening_positions': 0,
+            'closing_positions': 0,
+            'closed_positions': 0
+        }
     for _o in db.pairperf_column_settings.find():
-        markets[_o['market']] = {
-            'closed_positions': 0,
-            'gain_at_stoploss': 0,
-            'cumulated_gains': 0
-        }
-    markets['other'] = {
-            'closed_positions': 0,
-            'gain_at_stoploss': 0,
-            'cumulated_gains': 0
-        }
+        markets[_o['market']] = _skeleton
+    markets['other'] = _skeleton
 
     # Get all closed positions in last hour
     now = dt.datetime.utcnow()
+    now = dt.datetime(2020, 1, 23, 10, 0,0)
     for position in db.positions.find({"$and": [
         {"status": "closed"},
         {"closed_at": {"$gt": (now - dt.timedelta(minutes=60)), "$lte": now}}
@@ -75,20 +77,33 @@ try:
         ]);
         _gain_at_stop_loss = list(cursor)[0].get('sum')
 
-        if position['market'] in markets:
-            markets[position['market']] = {
-                'closed_positions': markets[position['market']]['closed_positions'] + 1,
-                'gain_at_stoploss': markets[position['market']]['gain_at_stoploss'] + _gain_at_stop_loss,
-                'cumulated_gains': markets[position['market']]['cumulated_gains'] + position['net']
-            }
-        else:
-            markets['other'] = {
-                'closed_positions': markets['other']['closed_positions'] + 1,
-                'gain_at_stoploss': markets['other']['gain_at_stoploss'] + _gain_at_stop_loss,
-                'cumulated_gains': markets['other']['cumulated_gains'] + position['net']
-            }
+        # Get position counts for this market
+        _open_positions = db.positions.count_documents({'status': 'open', 'market': position['market']})
+        _opening_positions = db.positions.count_documents({'status': 'opening', 'market': position['market']})
+        _closing_positions = db.positions.count_documents({'status': 'closing', 'market': position['market']})
+        _closed_positions = db.positions.count_documents({'status': 'closed', 'market': position['market']})
 
-    # Summarize stats per closed pair in last hour
+        if position['market'] in markets:
+            _key = position['market']
+        else:
+            _key = 'other'
+
+        markets[_key] = {
+            'closed_last_hour': markets[_key]['closed_positions'] + 1,
+            'gain_at_stoploss': markets[_key]['gain_at_stoploss'] + _gain_at_stop_loss,
+            'cumulated_gains': markets[_key]['cumulated_gains'] + position['net'],
+            'open_positions': _open_positions,
+            'opening_positions': _opening_positions,
+            'closing_positions': _closing_positions,
+            'closed_positions': _closed_positions
+        }
+
+    # Do not store zero values
+    # Cleansing:
+    for market, data in markets.items():
+        if (markets[market]['closed_last_hour'] == 0 and markets[market]['open_positions'] == 0
+            and markets[market]['opening_positions'] == 0 and markets[market]['closing_positions'] == 0):
+            del(markets[market])
 
     _doc = {
         "created_at": dt.datetime.utcnow(),
