ed2a2c387fe75e14ab63366903f6c079690aecb5
Fourat ZOUARI <fourat@gmail.com>
2020-07-08 11:00:27 +0100

    Added 24h, 1d and 1w gain stats in closure reports

--- reporter-closure.py
+++ reporter-closure.py
@@ -46,26 +46,29 @@ try:
     # Get market_settings
     markets = {}
     _skeleton = {
-            'closed_last_hour': 0,
-            'cumulated_gains_last_hour': 0,
-            'gain_at_stoploss': 0,
-            'cumulated_gains': 0,
-            'open_positions': 0,
-            'opening_positions': 0,
-            'closing_positions': 0,
-            'closed_positions': 0
-        }
+        'closed_last_hour': 0,
+        'cumulated_gains_last_hour': 0,
+        'gain_at_stoploss': 0,
+        'cumulated_gains': 0,
+        'open_positions': 0,
+        'opening_positions': 0,
+        'closing_positions': 0,
+        'closed_positions': 0,
+        '24h_gain': 0,
+        '1w_gain': 0,
+        '1m_gain': 0
+    }
     for _o in db.market_settings.find({"reporting": True}):
         markets[_o['market']] = _skeleton
     markets['other'] = _skeleton
 
     # Get all closed positions in last hour
-    _right = dt.datetime.utcnow()
-    _left = _right - dt.timedelta(minutes=60)
+    right = dt.datetime.utcnow()
+    left = right - dt.timedelta(minutes=60)
     #_left = dt.datetime(2020, 1, 23, 10, 0,0)
     for position in db.positions.find({"$and": [
         {"status": "closed"},
-        {"closed_at": {"$gt": _left, "$lte": _right}}
+        {"closed_at": {"$gt": left, "$lte": right}}
     ]}):
         # Get gain at stop loss for this market
         cursor = db.positions.aggregate([
@@ -98,6 +101,57 @@ try:
         else:
             _cumulated_gain = 0
 
+        # Get 24h gain for this market
+        _left = right - dt.timedelta(hours=24)
+        cursor = db.positions.aggregate([
+            {"$match": {'status': 'closed',
+                        'market': position['market'],
+                        'closed_at': {"$gt": _left, "$lte": right}}},
+            {"$group": {
+                "_id": None,
+                "sum": {"$sum": "$net"}
+            }}
+        ]);
+        _res = list(cursor)
+        if len(_res) > 0:
+            _24h_gain = _res[0].get('sum')
+        else:
+            _24h_gain = 0
+
+        # Get 1w gain for this market
+        _left = right - dt.timedelta(days=7)
+        cursor = db.positions.aggregate([
+            {"$match": {'status': 'closed',
+                        'market': position['market'],
+                        'closed_at': {"$gt": _left, "$lte": right}}},
+            {"$group": {
+                "_id": None,
+                "sum": {"$sum": "$net"}
+            }}
+        ]);
+        _res = list(cursor)
+        if len(_res) > 0:
+            _1w_gain = _res[0].get('sum')
+        else:
+            _1w_gain = 0
+
+        # Get 1m gain for this market
+        _left = right - dt.timedelta(days=31)
+        cursor = db.positions.aggregate([
+            {"$match": {'status': 'closed',
+                        'market': position['market'],
+                        'closed_at': {"$gt": _left, "$lte": right}}},
+            {"$group": {
+                "_id": None,
+                "sum": {"$sum": "$net"}
+            }}
+        ]);
+        _res = list(cursor)
+        if len(_res) > 0:
+            _1m_gain = _res[0].get('sum')
+        else:
+            _1m_gain = 0
+
         # Get position counts for this market
         _open_positions = db.positions.count_documents({'status': 'open', 'market': position['market']})
         _opening_positions = db.positions.count_documents({'status': 'opening', 'market': position['market']})
@@ -117,7 +171,10 @@ try:
             'open_positions': _open_positions,
             'opening_positions': _opening_positions,
             'closing_positions': _closing_positions,
-            'closed_positions': _closed_positions
+            'closed_positions': _closed_positions,
+            '24h_gain': _24h_gain,
+            '1w_gain': _1w_gain,
+            '1m_gain': _1m_gain,
         }
 
     # Do not store zero values
@@ -129,8 +186,8 @@ try:
 
     _doc = {
         "created_at": dt.datetime.utcnow(),
-        "from_datetime": _left,
-        "to_datetime": _right,
+        "from_datetime": left,
+        "to_datetime": right,
         "pairs": markets
     }
 
